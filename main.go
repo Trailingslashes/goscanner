b82b76879fc2ca433175a19b3b653774713ab7f4
trailingslashes <cp110@prontonmail.com>
2020-02-15 17:23:56 -0500

    added go routines and channels

--- main.go
+++ main.go
@@ -3,23 +3,48 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
 )
 
-func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
-
-	if err == nil {
-		fmt.Println("Connection Successful!")
-	}
-
-	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+func worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
-			// port is closed or filtered.
+			results <- 0
 			continue
 		}
 		conn.Close()
-		fmt.Printf("%d open\n", i)
+		results <- p
+	}
+}
+
+func main() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
 	}
 }
